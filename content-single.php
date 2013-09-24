f1a20ca60db5d8d185da3d1eef150cfc0c6786b8
Kelly Dwan <kelly.dwan@gmail.com>
2013-09-23 22:52:27 -0400

    Change meta text to match mockup format

--- content-single.php
+++ content-single.php
@@ -34,17 +34,17 @@
 			if ( ! flat_writer_categorized_blog() ) {
 				// This blog only has 1 category so we just need to worry about tags in the meta text
 				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'flat-writer' );
+					$meta_text = __( ' with tags %2$s.', 'flat-writer' );
 				} else {
-					$meta_text = __( 'Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'flat-writer' );
+					$meta_text = '';
 				}
 
 			} else {
 				// But this blog has loads of categories so we should probably display them here
 				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'flat-writer' );
+					$meta_text = __( ' in %1$s with tags %2$s.', 'flat-writer' );
 				} else {
-					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'flat-writer' );
+					$meta_text = __( ' in %1$s.', 'flat-writer' );
 				}
 
 			} // end check for categories on this blog
@@ -52,9 +52,7 @@
 			printf(
 				$meta_text,
 				$category_list,
-				$tag_list,
-				get_permalink(),
-				the_title_attribute( 'echo=0' )
+				$tag_list
 			);
 		?>
 
