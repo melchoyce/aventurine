e7256ab9772f1a5a6c4c220d5487b30b2b85991c
Kelly Dwan <kelly.dwan@gmail.com>
2013-09-23 23:02:54 -0400

    Featured image alignment. Always expand the image to fill the space, even if it's a tall image

--- content-single.php
+++ content-single.php
@@ -5,9 +5,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if ( has_post_thumbnail() ) {
-		the_post_thumbnail();
-	} ?>
+	<?php if ( has_post_thumbnail() ) : ?>
+		<div class="entry-image">
+		<?php the_post_thumbnail('full'); ?>
+		</div>
+	<?php endif; ?>
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
