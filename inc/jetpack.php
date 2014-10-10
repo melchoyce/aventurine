98ebe2c954ce1da8ade63cf983d3be9d62728d9d
ryelle <kelly.dwan@gmail.com>
2014-10-10 12:30:19 -0400

    Style infinite scroll credits & add a link to Aventurine.

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -17,3 +17,15 @@ function aventurine_jetpack_setup() {
 	) );
 }
 add_action( 'after_setup_theme', 'aventurine_jetpack_setup' );
+
+/**
+ * Credits
+ */
+function aventurine_infinite_scroll_credit( $credits ) {
+	$credits = '<a href="http://wordpress.org/" rel="generator">' . sprintf( __( 'Proudly powered by %s', 'aventurine' ), 'WordPress' ) . '</a>';
+	$credits .= '<span class="sep"> | </span>';
+	$credits .= '<a href="http://themes.redradar.net/">' . sprintf( __( 'Theme: %1$s', 'aventurine' ), 'Aventurine' ) . '</a>';
+
+	return $credits;
+}
+add_filter( 'infinite_scroll_credit', 'aventurine_infinite_scroll_credit' );
