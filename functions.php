d69a2b01aab1919ea75429005b86acffba2797b0
ryelle <kelly.dwan@gmail.com>
2014-12-15 19:06:09 -0500

    Rename “Sidebar” widget areas to “Footer”, add a description noting that they only appear on single views.

--- functions.php
+++ functions.php
@@ -71,16 +71,18 @@ add_action( 'after_setup_theme', 'aventurine_setup' );
  */
 function aventurine_widgets_init() {
 	register_sidebar( array(
-		'name'          => __( 'Left Sidebar', 'aventurine' ),
+		'name'          => __( 'Left Footer Widgets', 'aventurine' ),
 		'id'            => 'sidebar-1',
+		'description'   => __( 'Only appear on single posts &amp; pages', 'aventurine' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
 		'before_title'  => '<h1 class="widget-title">',
 		'after_title'   => '</h1>',
 	) );
 	register_sidebar( array(
-		'name'          => __( 'Right Sidebar', 'aventurine' ),
+		'name'          => __( 'Right Footer Widgets', 'aventurine' ),
 		'id'            => 'sidebar-2',
+		'description'   => __( 'Only appear on single posts &amp; pages', 'aventurine' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
 		'before_title'  => '<h1 class="widget-title">',
