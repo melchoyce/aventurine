5bcd54872767561863f3f8978c06d4d6d4db490c
Kelly Dwan <ryelle@users.noreply.github.com>
2020-09-26 11:21:29 -0400

    Use core `the_archive_*` functions

--- archive.php
+++ archive.php
@@ -15,83 +15,27 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					if ( is_category() ) :
-						single_cat_title();
-
-						elseif ( is_tag() ) :
-							single_tag_title();
-
-						elseif ( is_author() ) :
-							/*
-							 Queue the first post, that way we know
-							 * what author we're dealing with (if that is the case).
-							*/
-							the_post();
-							printf( __( 'Author: %s', 'aventurine' ), '<span class="vcard">' . get_the_author() . '</span>' );
-							/*
-							 Since we called the_post() above, we need to
-							 * rewind the loop back to the beginning that way
-							 * we can run the loop properly, in full.
-							 */
-							rewind_posts();
-
-						elseif ( is_day() ) :
-							printf( __( 'Day: %s', 'aventurine' ), '<span>' . get_the_date() . '</span>' );
-
-						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'aventurine' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
-
-						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'aventurine' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
-
-						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-							_e( 'Asides', 'aventurine' );
-
-						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-							_e( 'Images', 'aventurine' );
-
-						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-							_e( 'Videos', 'aventurine' );
-
-						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-							_e( 'Quotes', 'aventurine' );
-
-						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-							_e( 'Links', 'aventurine' );
-
-						else :
-							_e( 'Archives', 'aventurine' );
-
-						endif;
-						?>
-				</h1>
-				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-				if ( ! empty( $term_description ) ) :
-					printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
-				?>
+			<?php 
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="taxonomy-description">', '</div>' );
+			?>
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
 			<?php 
 			while ( have_posts() ) :
-				the_post(); 
-				?>
+				the_post();
 
-				<?php
-					/*
-					 Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a
+				 * file called content-___.php (where ___ is the Post Format name)
+				 * and that will be used instead.
+				 */
+				get_template_part( 'content', get_post_format() );
 
-			<?php endwhile; ?>
+			endwhile;
+			?>
 
 		<?php else : ?>
 
